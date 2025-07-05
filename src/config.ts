db148fd48e3ba352fd9f2325ec58a60ac081d465
yuichi10 <y_084743@yahoo.co.jp>
2025-07-05 13:41:03 +0900

    add token and url setting

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,6 @@ export const serverConfig = {
   port: config.get<number>("port"),
   server_mode: config.get<string>("server_mode"),
   stateful: config.get<boolean>("stateful"),
+  api_url: config.get<string>("api_url"),
+  token: config.get<string>("token"),
 };
